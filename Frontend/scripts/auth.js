68c832fb305e53938073a1cb461f6bdf9f7b28d3
Mark Nahano <marknahano@gmail.com>
2026-01-02 15:56:56 +0300

    Done with updating the Frontend

--- Frontend/scripts/auth.js
+++ Frontend/scripts/auth.js
@@ -13,7 +13,7 @@ const showLogin = () => {
     login_button_link.style.display = "none";
     signup_button_link.style.display = "flex";
   }
-}
+};
 
 function showSignup() {
   if (sign_up_box.style.display === "flex") {
@@ -30,16 +30,35 @@ login_button_link.addEventListener("click", showLogin);
 signup_button_link.addEventListener("click", showSignup);
 
 //Sign UP
-const signUpUserName = document.querySelector(".signup-username");
+const UserName = document.querySelector(".signup-username");
 
-const signUpEmail = document.querySelector(".signup-email");
-const signUpPassword = document.querySelector(".signup-password");
+const Email = document.querySelector(".signup-email");
+const PassWord = document.querySelector(".signup-password");
+const SubmitButton = document.querySelector("#sign_up_button");
+const signUpButton = document.querySelector(".sign-up-buttons");
 
-const signUpButton = document.querySelector(".sign-up-buttons") ;
 
-signUpButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log(signUpUserName.value);
-  console.log(signUpEmail.value);
-  console.log(signUpPassword.value );
+function get_user_data(id) {
+  const content = id.value.trim();
+  console.log(content)
+  return content
+}
+
+SubmitButton.addEventListener("click", () => {
+  const username = get_user_data(UserName);
+  const useremail = get_user_data(Email);
+  const userpassword = get_user_data(PassWord);
+  fetch("http://127.0.0.1:8001/authentication/SignUp", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      NAME: username,
+      EMAIL: useremail,
+      PASSWORD: userpassword,
+    }),
+  })
+  
+
 });
